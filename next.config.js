2a6961eb7af62509f5616ecfdc028289cb0a95b1
Abhirama Sonny <abhirama.sonny@gmail.com>
2026-01-07 21:53:50 -0600

    Revert webpack config (incompatible with Turbopack)

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-    };
-    return config;
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
