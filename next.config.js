28a18c45516f485ca59215319a1ed17714c45f66
Abhirama Sonny <abhirama.sonny@gmail.com>
2026-01-07 21:52:54 -0600

    Ensure PDFs are included in Next.js static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+    };
+    return config;
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
